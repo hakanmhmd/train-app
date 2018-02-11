ea31ff91ec486cd613a080a955b9b80c6d61901e
Hakan <hakanmhmd@yahoo.com>
2018-02-11 10:23:13 +0000

    Building the search fragment

--- app/src/main/java/com/hakanmehmed/trainapp/androidtrainapp/MainActivity.java
+++ app/src/main/java/com/hakanmehmed/trainapp/androidtrainapp/MainActivity.java
@@ -1,13 +1,11 @@
 package com.hakanmehmed.trainapp.androidtrainapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
@@ -17,7 +15,8 @@ public class MainActivity extends AppCompatActivity  {
     private FrameLayout mMainFrame;
 
     private LocationFragment locationFragment;
-    private ScheduleFragment scheduleFragment;
+    private SearchJourneyFragment searchJourneyFragment;
+    private SavedJourneyFragment savedJourneyFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +26,8 @@ public class MainActivity extends AppCompatActivity  {
         mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
         mMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
         locationFragment = new LocationFragment();
-        scheduleFragment = new ScheduleFragment();
+        searchJourneyFragment = new SearchJourneyFragment();
+        savedJourneyFragment = new SavedJourneyFragment();
         setFragment(locationFragment);
 
         mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,10 +37,13 @@ public class MainActivity extends AppCompatActivity  {
                     case R.id.nav_locate:
                         setFragment(locationFragment);
                         return true;
-                    case R.id.nav_schedule:
-                        setFragment(scheduleFragment);
+                    case R.id.nav_search_journey:
+                        setFragment(searchJourneyFragment);
 //                        Intent in = new Intent(getBaseContext(), ScheduleActivity.class);
 //                        startActivity(in);
+                        return true;
+                    case R.id.nav_saved_journey:
+                        setFragment(savedJourneyFragment);
                         return true;
                     default:
                         return false;
@@ -54,30 +57,6 @@ public class MainActivity extends AppCompatActivity  {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.main_frame, fragment);
         fragmentTransaction.commit();
-    }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
---
+++ app/src/main/java/com/hakanmehmed/trainapp/androidtrainapp/SearchJourneyFragment.java
@@ -0,0 +1,24 @@
+package com.hakanmehmed.trainapp.androidtrainapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SearchJourneyFragment extends Fragment {
+    public SearchJourneyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_search_journey, container, false);
+        return view;
+    }
+
+}
